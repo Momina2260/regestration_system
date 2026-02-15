99290928e436ad683c7e9e24ec5c59b84fc745c8
momina <momina.personal.2003@gmail.com>
2026-02-15 16:18:20 +0500

    security enhancement

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
+import os
+from dotenv import load_dotenv
 from flask import Flask
 from controllers.routes import routes
 
+load_dotenv()  # Load .env file
+
 app = Flask(__name__)
-app.secret_key = "supersecretkey"
+app.secret_key = os.getenv("SECRET_KEY")
 
 app.register_blueprint(routes)
 
 if __name__ == "__main__":
     app.run(debug=True)
-
